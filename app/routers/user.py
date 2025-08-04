84e9ee4445d93afecfab3eb994eceb4351bc458d
imadios <imadios@naver.com>
2025-08-04 15:47:11 +0900

    임포트 오류 해결

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends
 from app.dependencies.auth import get_current_user
-from app.dependencies.auth import get_current_user_from_cookie
+
 
 user_router = APIRouter()
 
 @user_router.get("/me/cookie")
-def get_me_cookie(user_id: str = Depends(get_current_user_from_cookie)):
+def get_me_cookie(user_id: str = Depends(get_current_user)):
     return {"message": "✅ 쿠키 인증 성공", "user_id": user_id}
 
 @user_router.get("/me/bearer")
